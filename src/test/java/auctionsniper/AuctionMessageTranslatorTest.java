de45031d44a5066d5004ce19543796a6f3d0841d
Florian Kempenich <Flori@nKempenich.com>
2020-02-18 15:55:55 +0000

    Add stub for AuctionMessageTranslator

---
+++ src/test/java/auctionsniper/AuctionMessageTranslatorTest.java
@@ -0,0 +1,12 @@
+package auctionsniper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuctionMessageTranslatorTest {
+    @Test
+    void notifiesAuctionClosedWhenCloseMessageReceived() {
+        // todo
+    }
+}
