2dfffcdc791d1b08fe877fd5dc721b8eb199e95e
Florian Kempenich <Flori@nKempenich.com>
2020-02-18 19:15:12 +0000

    Extract AuctionEvent

--- src/main/java/auctionsniper/AuctionMessageTranslator.java
+++ src/main/java/auctionsniper/AuctionMessageTranslator.java
@@ -17,29 +17,53 @@ public class AuctionMessageTranslator implements IncomingChatMessageListener {
 
     @Override
     public void newIncomingMessage(EntityBareJid from, Message message, Chat chat) {
-        Map<String, String> event = parse(message);
-        String eventType = event.get("Event");
-        if (eventType.equals("PRICE")) {
-            listener.currentPrice(
-                    Integer.parseInt(event.get("CurrentPrice")),
-                    Integer.parseInt(event.get("Increment"))
-            );
-        } else {
+        AuctionEvent event = AuctionEvent.from(message.getBody());
+        String eventType = event.type();
+
+        if ("CLOSE".equals(eventType)) {
             listener.auctionClosed();
+        }
+
+        if ("PRICE".equals(eventType)) {
+            listener.currentPrice(
+                    event.currentPrice(),
+                    event.increment()
+            );
         }
     }
 
-    private Map<String, String> parse(Message message) {
-        Map<String, String> parsed = new HashMap<>();
-        String body = message.getBody();
+    private static class AuctionEvent {
+        private Map<String, String> fields;
 
-        for (String part : body.split(";")) {
-            String[] keyVal = part.split(":");
-            String key = keyVal[0].trim();
-            String value = keyVal[1].trim();
-            parsed.put(key, value);
+        private AuctionEvent() { this.fields = new HashMap<>(); }
+
+        public static AuctionEvent from(String eventMsg) {
+            AuctionEvent event = new AuctionEvent();
+            for (String field : fieldsIn(eventMsg)) {
+                event.addField(field);
+            }
+            return event;
         }
 
-        return parsed;
+        private static String[] fieldsIn(String eventMsg) {
+            return eventMsg.split(";");
+        }
+
+        private void addField(String field) {
+            String[] keyVal = field.split(":");
+            String key = keyVal[0].trim();
+            String value = keyVal[1].trim();
+            fields.put(key, value);
+        }
+
+        public int increment() { return getInt("Increment"); }
+
+        private int getInt(String fieldName) { return Integer.parseInt(get(fieldName)); }
+
+        private String get(String fieldName) { return fields.get(fieldName); }
+
+        public String type() { return get("Event"); }
+
+        public int currentPrice() { return getInt("CurrentPrice"); }
     }
 }
