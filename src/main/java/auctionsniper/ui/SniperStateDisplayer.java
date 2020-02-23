eea2659af8687fec9b4fd2f82521e84c960896e1
Florian Kempenich <Flori@nKempenich.com>
2020-02-23 15:00:05 +0000

    Add comment explaining why we run Swing events on a different Thread

--- src/main/java/auctionsniper/ui/SniperStateDisplayer.java
+++ src/main/java/auctionsniper/ui/SniperStateDisplayer.java
@@ -14,6 +14,15 @@ public class SniperStateDisplayer implements SniperListener {
 
     @Override
     public void sniperStateChanged(SniperSnapshot snapshot) {
+        /* Why run on a special thread?
+
+        Swing event handling code runs on a special thread known as the event dispatch thread.
+        Most code that invokes Swing methods must also runs on this thread.
+        This is necessary because most Swing object methods are not "thread safe": invoking them
+        from multiple threads risks thread interference or memory consistency errors.
+
+        -> https://docs.oracle.com/javase/tutorial/uiswing/concurrency/dispatch.html
+         */
         SwingUtilities.invokeLater(() -> ui.sniperStateChanged(snapshot));
     }
 }
